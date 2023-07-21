7f90f8e60dd770e3ea1815690fa075b14de99d9f
AiDN <aidan.p1993@gmail.com>
2023-07-21 12:56:27 +0100

    Updated to move the test route to Mat Run 1
    Updated to move the test route to Mat Run 1
    
    Pickup and collection is pretty much a straight line from 8180 to ammunation 11
    
    Still need to add a couple more routes around the map

--- resources/[NGG]/ng-matrun/client.lua
+++ resources/[NGG]/ng-matrun/client.lua
@@ -1,24 +1,23 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local blip = AddBlipForCoord(116.44, -1088.16, 29.22) -- this is our mision bli
+local blip = AddBlipForCoord(802.93, -2135.77, 29.43) -- this is our mision bli
 local PackageCount = 0
 
---This Sets up the materials pickup and then handles everything to do with pickup
+--This Sets up the materials pickup 1 and then handles everything to do with pickup
 local Zone = nil
+
 CreateThread(function()
-    Zone = BoxZone:Create(vector3(156.47, -1065.58, 29.19), 1.4, 1, {
-        name="matPickup1",
-        heading=250,
+    Zone = BoxZone:Create(vector3(704.64, -1142.62, 23.61), 6.4, 7, {
+        name="matCollection1",
+        heading=0,
         debugPoly=true,
-        minZ=27.59,
-        maxZ=31.59,
-    })
+        minZ=20.01,
+        maxZ=27.21
+      })
+      
 
     Zone:onPlayerInOut(function(isPointInside)
         --local Player = PlayerPedId()
         if isPointInside and PackageCount == 0 then
-            Wait(1000)
-            TriggerServerEvent('addMatPackage')
-            PackageCount = 1
             DeliverPackages()
         else if PackageCount == 1 then
             Wait(1000)
@@ -30,21 +29,20 @@ end)
 
 --This sets up the materials drop off zone and handles everything at the drop off
 CreateThread(function()
-    Zone = BoxZone:Create(vector3(116.46, -1089.3, 29.23), 1.4, 1, {
-        name="matDropoff",
-        heading=92,
+    Zone = BoxZone:Create(vector3(803.01, -2134.89, 29.37), 9.2, 5, {
+        name="matDropoff1",
+        heading=0,
         debugPoly=true,
-        minZ=27.23,
-        maxZ=31.23
-    })
+        minZ=28.17,
+        maxZ=32.17
+      })
 
     Zone:onPlayerInOut(function(isPointInside)
         --local Player = PlayerPedId()
         if isPointInside and PackageCount == 1 then
             TriggerServerEvent('removeMatPackages')
-            Wait(1000)
-            playerMatReward()
             PackageCount = 0
+            playerMatReward()
         else 
             QBCore.Functions.Notify('Go get a package')
         end
@@ -53,6 +51,11 @@ end)
 
 --Player has to deliver materials packages to a dropoff, this generates a blip that the player can target for gps
 function DeliverPackages()
+    PackageCount = 1
+
+    Wait(1000)
+    TriggerServerEvent('addMatPackage')
+
     SetBlipSprite(blip, 286)
     SetBlipDisplay(blip, 2)
     SetBlipScale(blip, 1.0)
@@ -66,7 +69,9 @@ end
 --After the player has done the package drop off the player is then rewarded with gun parts which can be crafted into weapons later
 function playerMatReward()
     Wait(1000)
+
     QBCore.Functions.Notify('You have been rewarded with Gun Parts')
     TriggerServerEvent('playerMatReward')
+    
     RemoveBlip(blip)
 end
