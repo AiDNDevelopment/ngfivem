fa5f923f9c69ac64898990837d29719bbd80667e
AiDNDevelopment <aidan.p1993@gmail.com>
2023-08-08 22:20:52 +0100

    Continued work on the ng-customs package, so far handles Spoiler, FrontB, RearB, Exhaust, Roof, Skirts planning on adding more when im not dead

--- resources/[NG]/ng-customs/client/client.lua
+++ resources/[NG]/ng-customs/client/client.lua
@@ -1,105 +1,300 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local classTable = {
+    [0] = "Compacts",
+    [1] = "Sedans",
+    [2] = "SUVs",
+    [3] = "Coupes",
+    [4] = "Muscle",
+    [5] = "Sports Classics",
+    [6] = "Sports",
+    [7] = "Super",
+    [8] = "Motorcycles",
+    [9] = "Off-Road",
+    [10] = "Industrial",
+    [11] = "Utility"
+}
+
+--DEBUG PROBBALY CHANGE THIS BUT THIS CHECKS IF THE PLAYER IS PRESSING E
 CreateThread(function()
     while true do
         Wait(0)
         if IsControlJustReleased(0, 38) then
-            test()
+            print("getting vehicle")
+            getPlayerVeh()
         end
     end
 end)
 
-RegisterNetEvent('attachSpoilerClient')
-AddEventHandler('attachSpoilerClient', function(vehicle, spoilerIndex)
-    print('attachSpoilerClient called with vehicle:', vehicle, 'spoilerIndex:', spoilerIndex)
-    SetVehicleMod(vehicle, 0, spoilerIndex)
-end)
-
-
-function test()
-    local x, y, z = table.unpack(GetEntityCoords(PlayerPedId()))
-    local radius = 5.0 -- Change this to the radius in which you want to search for vehicles
-    local modelHash = 0 -- Set this to 0 to search for any vehicle model
-    local flags = 70 -- Set this to 70 to search for any vehicle type
-    local vehicle = GetClosestVehicle(x, y, z, radius, modelHash, flags)
-    if vehicle == 0 then
-        TriggerEvent('chat:addMessage', {
-            color = {255, 0, 0},
-            multiline = true,
-            args = {"Me", "No vehicle found!"}
-        })
-        return
-    end
-    openSpoilerMenu(vehicle)
+--#region FIND THE VEHICLE TO MOD
+function getClosestVehicleToPlayer()
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    return GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 70)
 end
 
+--GETS THE CLOSEST VEHICLE AND BEGINS TO CHECK IF ITS PLAYER OWNED
+function getPlayerVeh()
+    local closestVehicle = getClosestVehicleToPlayer()
+    local plate = GetVehicleNumberPlateText(closestVehicle)
+    TriggerServerEvent('checkVehicleOwnership',plate)
+    getPlayerVehInfo()
+end
 
-function openSpoilerMenu(vehicle)
-    local accept = exports["nh-context"]:ContextMenu({
+function getPlayerVehInfo()
+    local closestVehicle = getClosestVehicleToPlayer()
+    local model = GetEntityModel(closestVehicle)
+    local make = GetDisplayNameFromVehicleModel(model)
+    local makeName = GetLabelText(make)
+    local class = GetVehicleClass(closestVehicle)
+    local classString = classTable[class]
+
+    print("make ".. makeName)
+    print("class " .. classString)
+end
+
+RegisterNetEvent('checkVehicleOwnershipResult', function(isOwned)
+    if isOwned then
+        TriggerEvent("ng-customs:mainMenu")
+    else
+        print("Vehicle not registered by any player")
+    end
+end)
+--#endregion
+
+--#region EACH PART HAS ITS OWN MENU
+RegisterNetEvent("ng-customs:mainMenu", function()
+    TriggerEvent("nh-context:createMenu", {
         {
-            header = "Select Spoiler",
+            header = "Main Menu"
         },
         {
-            header = "Spoiler 1",
-            args = {"1"}
+            header = "Spoilers",
+            context = "Open Spoiler Selection",
+            event = "ng-customs:spoilerMenu"
         },
         {
-            header = "Spoiler 2",
-            args = {"2"}
+            header = "Bumper (F)",
+            context = "Open bumper (F) selection",
+            event = "ng-customs:fBumperMenu"
         },
         {
-            header = "Spoiler 3",
-            args = {"3"}
+            header = "Bumper (R)",
+            context = "Open bumper (R) selection",
+            event = "ng-customs:rBumperMenu"
         },
         {
-            header = "Spoiler 4",
-            args = {"4"}
+            header = "Skirts",
+            context = "Open skirt selection",
+            event = "ng-customs:skirtMenu"
         },
         {
-            header = "Spoiler 5",
-            args = {"5"}
+            header = "Exhausts",
+            context = "Open exhaust selection",
+            event = "ng-customs:exhaustMenu"
         },
         {
-            header = "Spoiler 6",
-            args = {"6"}
-        },
-        {
-            header = "Spoiler 7",
-            args = {"7"}
-        },
-        {
-            header = "Spoiler 8",
-            args = {"8"}
-        },
-        {
-            header = "Spoiler 9",
-            args = {"9"}
-        },
-        {
-            header = "Spoiler 10",
-            args = {"10"}
+            header = "Roofs",
+            context = "Open roof selection",
+            event = "ng-customs:roofMenu"
         },
     })
-    if accept ~= nil then
-        if accept == "1" then
-            TriggerEvent('attachSpoilerClient', vehicle, 1)
-        elseif accept == "2" then
-            TriggerEvent('attachSpoilerClient', vehicle, 2)
-        elseif accept == "3" then
-            TriggerEvent('attachSpoilerClient', vehicle, 3)
-        elseif accept == "4" then
-            TriggerEvent('attachSpoilerClient', vehicle, 4)
-        elseif accept == "5" then
-            TriggerEvent('attachSpoilerClient', vehicle, 5)
-        elseif accept == "6" then
-            TriggerEvent('attachSpoilerClient', vehicle, 6)
-        elseif accept == "7" then
-            TriggerEvent('attachSpoilerClient', vehicle, 7)
-        elseif accept == "8" then
-            TriggerEvent('attachSpoilerClient', vehicle, 8)
-        elseif accept == "9" then
-            TriggerEvent('attachSpoilerClient', vehicle, 9)
-        elseif accept == "10" then
-            TriggerEvent('attachSpoilerClient', vehicle, 10)
-        end
-    end
-end
+end)
 
+RegisterNetEvent("ng-customs:spoilerMenu", function()
+    local menu = {
+        {
+            header = "Spoilers"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setSpoiler",
+            args = {i}
+        })
+    end
+
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+
+RegisterNetEvent("ng-customs:fBumperMenu", function()
+    local menu = {
+        {
+            header = "Bumpers(F)"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setfBumper",
+            args = {i}
+        })
+    end
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+
+RegisterNetEvent("ng-customs:rBumperMenu", function()
+    local menu = {
+        {
+            header = "Bumpers(F)"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setrBumper",
+            args = {i}
+        })
+    end
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+
+RegisterNetEvent("ng-customs:skirtMenu", function()
+    local menu = {
+        {
+            header = "Skirts"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setSkirt",
+            args = {i}
+        })
+    end
+
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+
+RegisterNetEvent("ng-customs:exhaustMenu", function()
+    local menu = {
+        {
+            header = "Skirts"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setExhaust",
+            args = {i}
+        })
+    end
+
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+
+RegisterNetEvent("ng-customs:roofMenu", function()
+    local menu = {
+        {
+            header = "Skirts"
+        },
+        {
+            header = "< GO BACK",
+            event = "ng-customs:mainMenu"
+        },
+    }
+
+    for i = 1, 15 do
+        table.insert(menu, {
+            header = "Option " .. i,
+            event = "ng-customs:setChassis",
+            args = {i}
+        })
+    end
+
+    TriggerEvent("nh-context:createMenu", menu)
+end)
+--#endregion
+
+--#region SET PART SCRIPTS
+RegisterNetEvent("ng-customs:setSpoiler", function(args) 
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 70)
+    
+    if args >= 1 and args <= 15 then
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 0, args)
+    end    
+end)
+
+RegisterNetEvent("ng-customs:setfBumper", function(args)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.9, 0, 70)
+
+    if args >=1 and args <=15 then 
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 1, args)
+    end
+end)
+
+RegisterNetEvent("ng-customs:setrBumper", function(args)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.9, 0, 70)
+
+    if args >=1 and args <=15 then 
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 2, args)
+    end
+end)
+
+RegisterNetEvent("ng-customs:setSkirt", function(args) 
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 70)
+    
+    if args >= 1 and args <= 15 then
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 3, args)
+    end    
+end)
+
+RegisterNetEvent("ng-customs:setExhaust", function(args) 
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 70)
+    
+    if args >= 1 and args <= 15 then
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 4, args)
+    end    
+end)
+
+RegisterNetEvent("ng-customs:setRoof", function(args) 
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 70)
+    
+    if args >= 1 and args <= 15 then
+        print("Selected " .. args)
+        SetVehicleMod(vehicle, 5, args)
+    end    
+end)
+--#endregion
--- resources/[NG]/ng-customs/server/server.lua
+++ resources/[NG]/ng-customs/server/server.lua
@@ -1,4 +1,11 @@
-RegisterNetEvent('attachSpoiler')
-AddEventHandler('attachSpoiler', function(vehicle, spoilerIndex)
-    TriggerClientEvent('attachSpoilerClient', -1, vehicle, spoilerIndex)
+local QBCore = exports['qb-core']:GetCoreObject()
+
+--CHECKS THE LICENSE PLATE AGAINST THE DB AND CHECKS TO SEE IF ITS VALID
+RegisterNetEvent('checkVehicleOwnership')
+AddEventHandler('checkVehicleOwnership', function(plate)
+    local playerId = source
+    print("Player ID on server side: " .. playerId)
+    local result = exports.oxmysql:fetchSync('SELECT * FROM player_vehicles WHERE plate = ?', {plate})
+    local isOwned = result ~= nil
+    TriggerClientEvent('checkVehicleOwnershipResult', playerId, isOwned)
 end)
